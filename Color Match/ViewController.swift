b0b5079f3991d66e8b5c41ea8707b9fb242ecae4
RB Embleton <RBEmbleton@aol.com>
2016-09-08 23:14:35 -0400

    Initial Commit

---
+++ Color Match/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Color Match
+//
+//  Created by RB Embleton on 9/8/16.
+//  Copyright © 2016 RB Embleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
